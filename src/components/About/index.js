c9ed92eba565829b1162845e6f0f2c165bd045ac
UPye <Urika.Pye@gmail.com>
2022-02-24 12:15:24 -0500

    Updated About Me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,52 +9,29 @@ const About = () => (
             <h1> All About Me </h1>
         </div>
         <div className="body">
-            <p>
-                My name is Urika Pye, and I am a jack of all trades, literally. I'm experienced in customer service,
-                hospitality, valet, casino and hotel, restaurant, fast-food, retail, and department stores. I've also 
-                worked, and am working in the IT field, tech support, and warehousing. However, I have never really 
-                found a career path that challenges me on a daily basis, until I was introduced to learning my first 
-                coding language. Even though HTML and CSS are far from challenging, I felt like I found my calling 
-                in life, so I began to want to know more. First, I began at freecodecamp.org and many free learning 
-                to code apps; however, I was never disiplined enough to carve out time to study because there was always 
-                something life threw my way. When moving to Arkansas, I began wanting to know more about studying, so 
-                that's when I came across apprenticeships. I went through the steps with the Arkansas Workforce and 
-                Development office in Little Rock to begin the apprenticeship process. That's when I was enrolled in 
-                my first coding course at the University of Arkansas online called Creating Webpages. Believe me, I knew 
-                I was on my way to becoming a full on hacker from there. That's when I met the lovely Javascript, and 
-                this language definitely challenged me. DOM manipulation alone challenged me because I felt everything I 
-                learned and did was in vain. That's when I realized, Javascript synthax doesn't like me. However, by the 
-                time I enrolled in UCF's coding bootcamp, I was surrounded by a community of peers working towards the 
-                same goal, and they all played a viable part in me realizing Javascript runs the world. It always has and 
-                it always will, so I may as well learn it now. 
-            </p>
-            <p> 
-                Once we got to API calls, I began thinking, what did I get myself into. I usually pick up things super 
-                fast, but coding always had something else planned time-wise around the corner. While working 40+ hours 
-                a week and being promoted, I also juggled coding 30+ hours a week. Even though I was on the part-time 
-                schedule, it felt like I was doing full-time. Working to complete 2 builds a week was the bain of my existence. 
-                That is until everything started to make since. All the languages we were learning week in and week out 
-                all worked together like some sort of huge puzzle. All the synthax followed the same format, with different 
-                synthax, no matter the language. That's when I realized coding is EXACTLY like math, as far as the way you 
-                think and work through the process of solving problems. You can get from point A to point B multiple ways, 
-                rather you're using PHP, React, Javascript, or any other laguage, the outcome will still be the same. On that 
-                day, I knew I would code for the rest of my life. That's when I began looking into career opportunities because 
-                something had to give. I was done bouncing from field to field to be apart of the highest bidder. I wanted to 
-                be apart of something larger than myself, and technology adds to the growth and development of life as we know 
-                it. Anyways, enough about my spill on how I came to love coding, and more about me.
-            </p>
-            <p>
-                I am a single woman without children, but I am the parent of a fur baby. I love trying new things, bowling, 
-                kayaking, and anything that can get my adrenaline going that isn't too dangerous. I would love to continue 
-                traveling and seeing the world, so I would love to work in a remote position. I also value paid time off, so 
-                after grueling projects or just a long run of projects, I may take a breather and not burn out. I also 
-                want to make at least $72K starting out, and I would like access to health/vision/dental insurance, short-term/
-                long-term disability, 401K matching (willing to negotiate up to limit), life insurance, and someone whom with I 
-                can still complete personal projects, without compromising the integrity of the company. Thank you for you time 
-                and your attention, feel free to poke around my portfolio website, so you can see more about how my work turns out. 
-                If you have any questions, visit my Contact Page to get my contact info or send me an email. I also take on one-time 
-                projects. 
-            </p>
+       <p>
+          My name is Urika Pye, and I am currently an Account Resolution Specialist II at Wells Fargo. 
+          I worked my way from a lower-level agent to a supervisor in less than 5 months, in 
+          my previous position. Thus, this demonstrates my determination for quick advancement 
+          through application of knowledge gathered along the way. I was the newest supervisor 
+          to the project, and I produced the highest scoring team when being audited. I was able 
+          to produce multiple high scoring agents, along with a high team average. I am also a 
+          proud owner of a fur baby. He is my joy and my drive in everything I do. 
+      </p>
+      <p>
+          I am looking to show-off my newfound knowledge from my newly acquired certification 
+          from the University of Central Florida in Software Development. This portfolio will 
+          demonstrate a wide array of languages learned in this certification program. I am 
+          sure the React component is obvious at this point, and I am looking further to diving
+          deeper into the abundance of knowledge the software developing have to offer.
+      </p>
+      <p>
+          In my personal time, I enjoy bowling, going to see a movie ( of pretty much any genre ), 
+          kayaking, and most of all traveling. I have visited ( along with my fur baby ), Tennessee, 
+          South Carolina, North Carolina, Mississippi, Arkansas, Texas ( without my fur baby ), 
+          Georgia, Alabama, and Florida. I have also visited, Virginia, Pennsylvania, Louisiana, 
+          and Missouri. We have many more places we would like to go see, including internationally.
+      </p>
         </div>
     </div>
 );
